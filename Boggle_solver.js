4d9635770e5a4151cd4940cc5e959e051fcf02a5
Metbcy <Amirbredy1@gmail.com>
2022-09-21 14:40:35 -0400

    Update Boggle_solver.js

--- Boggle_solver.js
+++ Boggle_solver.js
@@ -5,7 +5,144 @@
  * @param {string[]} dictionary - The list of available words.
  * @returns {string[]} solutions - Possible solutions to the Boggle board.
  */
- 
+ exports.findAllSolutions = function(grid, dictionary) {
+    let solutions = [];
+
+    if(grid == null || dictionary == null)
+       return solutions;
+  
+     let N = grid.length;
+     for(let i = 0; i < N; i++) {
+        if(grid[i].length != N ){
+  
+            return solutions;
+        }
+     }
+  
+    LowerCase(grid, dictionary);
+  
+  
+     if(!GridValidator(grid)){
+        return solutions;
+     }
+  
+    let solutionSet = new Set();
+  
+    let hash = createHashMap(dictionary);
+  
+    for(let y = 0; y < N; y++){
+  
+        for(let x = 0; x < N; x++){
+             let word = "";
+  
+             let visited = new Array(N).fill(false).map(() => new Array(N).fill(false));
+  
+             PrintAllWords(word, y, x, grid, visited, hash, solutionSet);
+        }
+  
+    }
+  
+    solutions = Array.from(solutionSet);
+  
+    return solutions;
+  }
+  
+  PrintAllWords = function(word, y, x, grid, visited, hash, solutionSet){
+  
+      let adjMatrix = [[-1, -1],
+                     [-1, 0],
+                     [-1, 1],
+                     [0, 1],
+                     [1, 1],
+                     [1, 0],
+                     [1,-1],
+                     [0, -1]];
+  
+    if (y < 0 || x < 0 || y >= grid.length || x >= grid.length || visited[y][x] == true)
+           return;
+  
+    word += grid[y][x];
+  
+    if(WordChecker(word, hash)) {
+           visited[y][x] = true;
+  
+           if(isWord(word, hash)) {
+               if(word.length >= 3)
+                    solutionSet.add(word);
+           }
+  
+           for(let i = 0; i < 8; i++){
+            PrintAllWords(word, y + adjMatrix[i][0], x + adjMatrix[i][1], grid, visited, hash, solutionSet);
+            }
+  
+    }
+  
+    visited[y][x] = false;
+  }
+  
+  WordChecker = function(word, hash) {
+     return hash[word] != undefined;
+  }
+  
+  
+  isWord = function(word, hash) {
+    return hash[word] == 1;
+  }
+  
+  createHashMap = function(dictionary){
+    var dict = {};
+    for(let i = 0; i < dictionary.length ; i++){
+      dict[dictionary[i]]= 1;
+      let wordlength = dictionary[i].length;
+      var str = dictionary[i];
+      for(let j = wordlength; wordlength > 1; wordlength--){
+        str = str.substr(0,wordlength-1);
+        if(str in dict){
+          if(str == 1 ){
+            dict[str]= 1;
+          }
+        }
+        else{
+          dict[str]= 0;
+        }
+      }
+    }
+    return dict;
+  }
+  
+  LowerCase = function(grid, dict){
+  
+      for(let i = 0; i < grid.length; i++) {
+        for( let j = 0; j < grid[i].length; j++) {
+             grid[i][j] = grid[i][j].toLowerCase();
+        }
+      }
+  
+      for(let i = 0; i < dict.length; i++) {
+        dict[i] = dict[i].toLowerCase();
+      }
+  }
+  
+    GridValidator = function(grid){
+      raexp = /(st|qu)|[a-prt-z]/;
+      for(let i = 0; i < grid.length; i++) {
+        for( let j = 0; j < grid[i].length; j++) {
+            if(!grid[i][j].match(raexp)){
+                 return false;
+            }
+        }
+  
+      }
+  
+      return true;
+  }
+  
+/*
+let A = 4;
+let B = 4;
+let D=26;
+
+                   
 class TrieTree
 {
     constructor()
@@ -16,14 +153,6 @@ class TrieTree
             this.child[i]=null;
     }
 }
-
-exports.findAllSolutions = function(grid, dictionary) {
-let A = 4;
-let B = 4;
-let D=26;
-
-                   
-
 
 function LWSearch1(root,Key)
 {
@@ -50,10 +179,10 @@ function location(i,l,done)
     /* function LWsearch(str) {
       for (var i=0; i < C; i++) if (str==dictionary[i]) return true;
       return false;
-    } */
+    } 
 function PrintAllWords(root,grid, i, l, done, str){
     if(root.parent == true)
-        console.log(str+"\n");
+        console.log(str+"<br>");
 
     if(location(i,l,done)){
         done[i][l] = true;
@@ -109,7 +238,7 @@ function PrintAllWords(root,grid, i, l, done, str){
 
     }
 
-function findAllSolutions(grid,root){
+function getwords(grid,root){
     let done = new Array(A);
     for (let i=0;i<A;i++)
     {
@@ -162,24 +291,24 @@ function findAllSolutions(grid,root){
     */
 
 
-}
+
   
 let dictionary = ["ART", "EGO", "GENT", "GET", "NET", "NEW", "NEWT", "PRAT",
 "PRY", "QUA", "QUART", "QUARTZ", "RAT", "TAR", "TARP",
 "TEN", "WENT", "WET", "ARTY", "EGG", "NOT", "QUAR"];
-    
-let root = new TrieTree();
+
+/* let root = new TrieTree();
 
 let C = dictionary.length;
 for(let i=0; i<C; i++)
     LWSearch1(root, dictionary[i]);
+*/
 
 let grid = [['T', 'W', 'Y', 'R'],
 ['E', 'N', 'P', 'H'],
 ['G', 'Z', 'QU', 'R'],
 ['St', 'N', 'T', 'A']];
   
-console.log(exports.findAllSolutions(grid,root))
+console.log(exports.findAllSolutions(grid, dictionary));
   // take the first char and check if the first char is in the dictionary...but it doesnt satistfy the 3 letters...then we iterate through the board, so then we try AB and we see AB is in the dictionary we go through the list to see it's not there 
   // Credit for coding inspiration GeeksforGeeks
-  // Needs more tweaking and work but need more time to complete.
